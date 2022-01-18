dd25b53ed804399cb75b4b19ec5c9a574651c702
officer hamster <malte.fial@hfg-gmuend.de>
2022-01-18 12:00:07 +0100

    experimente

---
+++ malte_spielewiese/spielewiese/sketch.js
@@ -0,0 +1,91 @@
+// Benedikt Groß
+// Example for matter-attractors an attractors plugin for matter.js
+// https://github.com/liabru/matter-attractors
+
+Matter.use('matter-attractors');
+
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Mouse = Matter.Mouse;
+const MouseConstraint = Matter.MouseConstraint;
+const Composites = Matter.Composites;
+
+let backgroundColor = "blue";
+let terrainColor = "darkblue";
+let sun_moonColor = "#EBE0C5";
+
+let canvasW = 1280;
+let canvasH = 720*7;
+let viewportW = 1280;
+let viewportH = 720;
+
+const drawBody = Helpers.drawBody;
+const drawBodies = Helpers.drawBodies;
+
+let engine;
+let attractor;
+let boxes;
+
+
+function setup() {
+  const canvas = createCanvas(800, 600);
+  
+  // create an engine
+  engine = Engine.create();
+  const world = engine.world;
+  // no gravity
+  // engine.world.gravity.scale = 0;
+
+
+  // add attractor
+  attractor = Bodies.circle(400, 400, 50, {
+    isStatic: false,
+    plugin: {
+      attractors: [
+        function(bodyA, bodyB) {
+          return {
+            x: (bodyA.position.x - bodyB.position.x) * 1e-6,
+            y: (bodyA.position.y - bodyB.position.y) * 1e-6,
+          };
+        }
+      ]
+    }
+  });
+  World.add(engine.world, attractor);
+
+  // add boxes
+  // xx, yy, columns, rows, columnGap, rowGap
+  boxes = Composites.stack(width/2, 0, 3, 20, 3, 3, function(x, y) {
+    return Bodies.circle(x, y, 10);
+  });
+  World.add(engine.world, boxes);
+
+  terrain_1 = new BlockCore(world,
+    { x: viewportW*1/5, y: 620, w: viewportW, h: viewportH/4, color: "white"},
+    { isStatic: true }
+  );
+
+  // run the engine
+  Engine.run(engine);
+}
+
+function draw() {
+  background(0);
+
+  if (mouseIsPressed) {
+    // smoothly move the attractor body towards the mouse
+    Body.translate(attractor, {
+      x: (mouseX - attractor.position.x) * 0.25,
+      y: (mouseY - attractor.position.y) * 0.25
+    });
+  }
+  terrain_1.draw()
+  stroke(128);
+  strokeWeight(1);
+  fill(255);
+  drawBodies(boxes.bodies);
+  drawBody(attractor);
+}
