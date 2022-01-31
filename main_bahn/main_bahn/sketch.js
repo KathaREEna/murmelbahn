351893e3db60a31ce5c5f7095060b51c4e39d323
officer hamster <malte.fial@hfg-gmuend.de>
2022-01-31 13:18:54 +0100

    canvas verlängert

--- main_bahn/main_bahn/sketch.js
+++ main_bahn/main_bahn/sketch.js
@@ -6,7 +6,7 @@ Matter.use('matter-attractors');
 let marblin;
 let marblinLover;
 let canvasW = 1280;
-let canvasH = 720 * 11;
+let canvasH = 720 * 12;
 let viewportW = 1280;
 let viewportH = 720;
 let frameR = 60;
@@ -148,7 +148,7 @@ function preload() {
     color: color(houseactualR,houseactualG,houseactualB)
   });
 
-  let level4position = viewportH * 7.5;
+  let level4position = viewportH * 8.5;
   lamp = new PolygonFromSVG(world, {
     x: viewportW / 2 +200,
     y: level4position+200,
@@ -210,8 +210,8 @@ function setup() {
 
   // create Main Character MURMEL
   marblin = new Magnet(world, {
-    x: viewportW/2,
-    y: 3050,
+    x: 350,
+    y: 50,
     r: 40,
     color: 'white',
     attraction: 0.25e-5
@@ -853,7 +853,7 @@ Matter.Events.on(engine, 'collisionStart', function(event) {
 
 
   // create level 5 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  let level7position = viewportH * 10.5;
+  let level7position = viewportH * 12.5;
 
   terrain_6 = new BlockCore(world, {
     x: 273,
@@ -894,88 +894,92 @@ Matter.Events.on(engine, 'collisionStart', function(event) {
     density: 100
   });
 
-  //create lampe level
+  //create lampe level + 2*viewportH
+
+
+  let levelmover = viewportH
+  
   marblinTest = new Ball(
     world,
-    { x: 1320, y: 5050, r: 40, color: 'red'},
+    { x: 1320, y: 5050+ levelmover, r: 40, color: 'red'},
     { isStatic: false, friction: 0 }
   );
 
   marblinTest2 = new Ball(
     world,
-    { x: 640, y: 5000, r: 40, color: 'black'},
+    { x: 640, y: 5000+ levelmover, r: 40, color: 'black'},
     { restitution: 0, friction: 0 }
   );
 
   loverRamp = new Block(
     world,
-    { x: 1160, y : 5480, w: 600, h: 10, color: 'yellow' },
+    { x: 1160, y : 5480+ levelmover, w: 600, h: 10, color: 'yellow' },
     { isStatic: true, label: 'loverRamp', angle: radians(-45) }
   );
 
   loverPlain = new Block(
     world,
-    { x: 1330, y : 5200, w: 100, h: 10, color: 'red' },
+    { x: 1330, y : 5200+ levelmover, w: 100, h: 10, color: 'red' },
     { isStatic: true, label: 'loverPlain' }
   );
 
   firstPlain = new Block(
     world,
-    { x: 640, y : 5620, w: 100, h: 10, color: 'red' },
+    { x: 640, y : 5620+ levelmover, w: 100, h: 10, color: 'red' },
     { isStatic: true, label: 'firstPlain' }
   );
 
   firstRamp = new Block(
     world,
-    { x: 710, y : 5655, w: 180, h: 10, color: 'red' },
+    { x: 710, y : 5655+ levelmover, w: 180, h: 10, color: 'red' },
     { isStatic: true, label: 'firstRamp', angle: radians(22) }
   );
 
   secondRamp = new Block(
     world,
-    { x: 910, y : 5730, w: 100, h: 10, color: 'red' },
+    { x: 910, y : 5730+ levelmover, w: 100, h: 10, color: 'red' },
     { isStatic: true, label: 'secondRamp', angle: radians(-45) }
   );
 
   secondPlain = new Block(
     world,
-    { x: 690, y : 5795, w: 450, h: 10, color: 'blue' },
+    { x: 690, y : 5795+ levelmover, w: 450, h: 10, color: 'blue' },
     { isStatic: true, label: 'secondPlain' }
   );
 
   thirdPlain = new Block(
     world,
-    { x: 308, y : 5795, w: 314, h: 10, color: 'red' },
+    { x: 308, y : 5795+ levelmover, w: 314, h: 10, color: 'red' },
     { isStatic: true, label: 'thirdPlain' }
   );
 
   blueWall = new Block(
     world,
-    { x: 30, y : 5700, w: 300, h: 10, color: 'blue' },
+    { x: 30, y : 5700+ levelmover, w: 300, h: 10, color: 'blue' },
     { isStatic: true, label: 'blueWall', angle: radians(90) }
   );
 
   bluePlain = new Block(
     world,
-    { x: 50, y : 5795, w: 100, h: 10, color: 'blue' },
+    { x: 50, y : 5795+ levelmover, w: 100, h: 10, color: 'blue' },
     { isStatic: true, label: 'bluePlain' }
   );
 
   bluePlain2 = new Block(
     world,
-    { x: 126, y : 5795, w: 50, h: 10, color: 'red' },
+    { x: 126, y : 5795+ levelmover, w: 50, h: 10, color: 'red' },
     { isStatic: true, label: 'bluePlain2' }
   );
 
   underplain_left = new Block(
     world,
-    { x: 308, y : 5796, w: 314, h: 10, color: 'pink' },
+    { x: 308, y : 5796+ levelmover, w: 314, h: 10, color: 'pink' },
     { isStatic: true, label: 'underplain_left' }
   );
 
   underplain_right = new Block(
     world,
-    { x: 690, y : 5796, w: 450, h: 10, color: 'green' },
+    { x: 690, y : 5796+ levelmover, w: 450, h: 10, color: 'green' },
     { isStatic: true, label: 'underplain_right' }
   );
 
@@ -1170,8 +1174,8 @@ function draw() {
   terrain_10_links.draw();
   // marblin.draw();
   // marblin.attract();
-  levelMarblin.draw();
-  levelMarblin.attract();
+  // levelMarblin.draw();
+  // levelMarblin.attract();
 
   stair4attractor.attract();
   stair5attractor.attract();
